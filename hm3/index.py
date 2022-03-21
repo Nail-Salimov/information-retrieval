67fe58c0cb80a32f5abc55e23a049b749313f6be
saydar31 <ajdarshaydullin@gmail.com>
2022-03-21 23:49:56 +0300

    fix finally

--- hm3/index.py
+++ hm3/index.py
@@ -27,17 +27,17 @@ def get_words_from_html(path):
     text = '\n'.join(chunk for chunk in chunks if chunk)
 
     # разбить слова по символам (split)
-    dirty_words = set(re.split(' |\n|«|»|,|:|–|-|}|{', text))
+    dirty_words = list(re.split(' |\n|«|»|,|:|–|-|}|{', text))
 
     r = re.compile("[а-яА-Я]+")
     clear_words = [w for w in filter(is_russian_word, dirty_words)]
     # word = re.sub('[^0-9А-Яa-я]+', '', str)
     clear_words = [re.sub('[^0-9А-Яa-я]+', ' ', w).strip() for w in clear_words]
-    return_words = set()
+    return_words = list()
     for word in clear_words:
         s = word.split()
-        return_words.update(tuple(s))
-    return return_words
+        return_words.extend(tuple(s))
+    return [s.lower() for s in return_words]
 
 
 lemmas_file = open("../hm2/lemmas.txt", "r", encoding='utf-8')
